10395218fa6d695e8f65883ddc0e7ca2bb091019
Andrés Alberto Ayala Flórez - Ceiba Software <andres.ayala@ceiba.com.co>
2020-02-05 10:02:14 -0500

    Enable CORS

--- Back/ceiba-adn-nest-api/src/main.ts
+++ Back/ceiba-adn-nest-api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => console.log('Listening on port 3000'));
+  // Enable CORS
+  app.enableCors();
+  await app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 }
 bootstrap();
