efc9046775b274e5bc30e18ee32bee05605c02be
Andrés Alberto Ayala Flórez - Ceiba Software <andres.ayala@ceiba.com.co>
2020-02-07 13:24:33 -0500

    All use cases with Unit Testing

---
+++ Back/ceiba-adn-nest-api/src/application/useCases/getAllTasks.usecase.spec.ts
@@ -0,0 +1,40 @@
+import TaskDto from "src/domain/dto/task.dto";
+import { TaskService } from "../../domain/services/task.service";
+import { TestingModule, Test } from "@nestjs/testing";
+
+const task: TaskDto = {
+  id: "5e39c3384caa9d380437f5cb",
+  taskName: "Tarea 5",
+  taskDescription: "Description",
+  status: "To Do",
+  taskDueDate: new Date(),
+  taskCreationDate: new Date(),
+  taskCompletionDate: new Date(),
+  pay: 18
+}
+
+describe('createTaskUseCase Unit Test', () => {
+  let service: TaskService;
+
+  const eventModel = {
+    getAllTasks: jest.fn().mockResolvedValue([task])
+  }
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        TaskService,
+        {
+          provide: 'TaskRepository',
+          useValue: eventModel,
+        },
+      ],
+    }).compile();
+
+    service = module.get<TaskService>(TaskService);
+  });
+
+  it('createTask', () => {
+    expect(service.getAllTasks()).resolves.toStrictEqual([task]);
+  })
+}) 
